1d368f9052462b9d3a0bf6ce933b760d08489be3
xWoZnIcAx <woznica2503@gmail.com>
2023-05-19 10:22:45 +0200

    Initial commit

---
+++ Zadanie 00/Zadanie 00.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Moja Strona</title>
+    <link rel="stylesheet" href="style.css"
+</head>
+<body>
+    <h1>Witaj na mojej stronie!</h1><hr>
+    <?php
+        echo "<b>Witaj świecie!<b>"
+    ?>
+</body>
+</html>
